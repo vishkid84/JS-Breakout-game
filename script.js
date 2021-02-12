5f1101b224e59b47bbdb9c9c7904c84b266a1d46
vishkid84 <vishalittr@gmail.com>
2021-02-12 16:53:41 +0000

    Added paddle

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 let canvas = document.getElementById('breakoutCanvas');
 let ctx = canvas.getContext('2d');
 
-setInterval(draw, 10);
-
 let x = canvas.width/2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 let ballRadius = 10;
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth)/2;
+let rightPressed = false;
+let leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler);
+document.addEventListener("keyup", keyUpHandler)
+
+function keyDownHandler(e){
+    if(e.keyCode == 39){
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37){
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e){
+    if(e.keyCode == 39){
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37){
+        leftPressed = false;
+    }
+}
 
 function drawBall(){
     ctx.beginPath();
@@ -17,9 +41,18 @@ function drawBall(){
     ctx.closePath();
 }
 
+function drawPaddle(){
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight)
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
+}
+
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     drawBall();
+    drawPaddle();
 
     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius){
         dy = -dy;
@@ -29,9 +62,18 @@ function draw(){
         dx = -dx;
     }
 
+    if(rightPressed && paddleX < canvas.width-paddleWidth){
+        paddleX += 7;
+    }
+    else if(leftPressed && paddleX > 0){
+        paddleX -= 7;
+    }
+
     x += dx;
     y += dy;
 }
+
+setInterval(draw, 10);
 
 
 
