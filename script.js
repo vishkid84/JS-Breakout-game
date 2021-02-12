f66cd713c6a561d15687f4f0316a0124d65913bc
vishkid84 <vishalittr@gmail.com>
2021-02-12 13:11:41 +0000

    Added function to bounce on corners

--- script.js
+++ script.js
@@ -1,20 +1,60 @@
 let canvas = document.getElementById('breakoutCanvas');
 let ctx = canvas.getContext('2d');
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = '#ff0000';
-ctx.fill();
-ctx.closePath();
+setInterval(draw, 10);
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = 'green';
-ctx.fill();
-ctx.closePath();
+let x = canvas.width/2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
+let ballRadius = 10;
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = 'rgba(0, 0, 255, 0.5)';
-ctx.stroke();
-ctx.closePath();
+function drawBall(){
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 10, 0, Math.PI*2);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    drawBall();
+
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius){
+        dy = -dy;
+    }
+
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius){
+        dx = -dx;
+    }
+
+    x += dx;
+    y += dy;
+}
+
+
+
+
+
+
+
+
+//---------- Some basic shapes-----------
+// ctx.beginPath();
+// ctx.rect(20, 40, 50, 50);
+// ctx.fillStyle = '#ff0000';
+// ctx.fill();
+// ctx.closePath();
+
+// ctx.beginPath();
+// ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+// ctx.fillStyle = 'green';
+// ctx.fill();
+// ctx.closePath();
+
+// ctx.beginPath();
+// ctx.rect(160, 10, 100, 40);
+// ctx.strokeStyle = 'rgba(0, 0, 255, 0.5)';
+// ctx.stroke();
+// ctx.closePath();
