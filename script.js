f3a61ccd3953a4052ac56897f3c52535f7866a9d
vishkid84 <vishalittr@gmail.com>
2021-02-16 12:54:57 +0000

    Added collision detection for bricks

--- script.js
+++ script.js
@@ -11,9 +11,43 @@ let paddleWidth = 75;
 let paddleX = (canvas.width - paddleWidth)/2;
 let rightPressed = false;
 let leftPressed = false;
+let brickRowCount = 3;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+
+let bricks = [];
+
+for(c=0; c<brickColumnCount; c++){
+    bricks[c] = [];
+    for(r=0; r<brickRowCount; r++){
+        bricks[c][r] = {x:0, y:0, status:1};
+    }
+}
 
 document.addEventListener("keydown", keyDownHandler);
 document.addEventListener("keyup", keyUpHandler)
+
+function drawBricks(){
+    for(c=0; c<brickColumnCount; c++){
+        for(r=0; r<brickRowCount; r++){
+            if(bricks[c][r].status == 1){
+                let brickX = (c*(brickWidth+brickPadding)) + brickOffsetLeft;
+                let brickY = (r*(brickHeight+brickPadding)) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath()
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = '#0095DD';
+                ctx.fill()
+                ctx.closePath()
+            }
+        }
+    }
+}
 
 function keyDownHandler(e){
     if(e.keyCode == 39){
@@ -49,15 +83,39 @@ function drawPaddle(){
     ctx.closePath();
 }
 
+function collisionDetection(){
+    for(c=0; c<brickColumnCount; c++){
+        for(r=0; r<brickRowCount; r++){
+            let b = bricks[c][r];
+            if(b.status == 1){
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight){
+                    dy = -dy;
+                    b.status = 0;
+                }
+            }
+        }
+    }
+}
+
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     drawBall();
+    drawBricks();
     drawPaddle();
+    collisionDetection();
 
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius){
+    if(y + dy < ballRadius){
         dy = -dy;
     }
-
+    else if(y + dy > canvas.height-ballRadius){
+        if(x > paddleX && x < paddleX + paddleWidth){
+            dy = -dy;
+        }
+        else{
+            alert("GAME OVER");
+            document.location.reload();
+        }
+    }
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius){
         dx = -dx;
     }
